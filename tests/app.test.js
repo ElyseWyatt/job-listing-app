3d941f9b8647c304abbeb1fb02378279661d8c0b
Elyse Wyatt <hi.elyse.wyatt@gmail.com>
2019-07-27 11:38:15 +1200

    updates.

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 //import Enzyme, { shallow } from 'enzyme'
 //import Adapter from 'enzyme-adapter-react-16'
 
-import App from '../client/components/App'
+//import App from '../client/components/App'
 
 //Enzyme.configure({adapter: new Adapter()})
 
